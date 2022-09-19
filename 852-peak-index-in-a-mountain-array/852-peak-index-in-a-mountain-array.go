a57c21eb6c0267e5efa6953d8099ece1ad93472e
MaltsevaNata <42980988+MaltsevaNata@users.noreply.github.com>
2022-09-19 22:03:28 +0300

    Time: 98 ms (92.59%), Space: 9.9 MB (42.59%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.go
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.go
@@ -1,16 +1,8 @@
 func peakIndexInMountainArray(arr []int) int {
-	max_idx := len(arr) - 1
-	var res_idx int
 	for idx, val := range arr {
-		if idx < max_idx {
-			if val > arr[idx+1] {
-				res_idx = idx
-				break
-			}
-		} else {
-			res_idx = idx
-			break
-		}
+        if val > arr[idx+1] {
+            return idx
+        }
 	}
-	return res_idx
+    return 1
 }
