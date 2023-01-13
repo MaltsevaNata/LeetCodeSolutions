a596799d3a1032273fb49beaf7512d2ee0ecb673
MaltsevaNata <42980988+MaltsevaNata@users.noreply.github.com>
2023-01-13 17:49:25 +0300

    Time: 0 ms (100.00%), Space: 2.5 MB (100.00%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.go
+++ 0205-isomorphic-strings/0205-isomorphic-strings.go
@@ -1,15 +1,10 @@
-func getLettersCode(str string) (code []int) {
-	letterCodeMap := make(map[int32]int) // {letter: code}
-	for ind, char := range str {
-		letterCode, ok := letterCodeMap[char]
-		if !ok {
-			letterCodeMap[char] = ind
-			letterCode = ind
-		}
-		code = append(code, letterCode)
+func getLettersCode(char int32, ind int, letterCodeMap map[int32]int) int {
+	letterCode, ok := letterCodeMap[char]
+	if !ok {
+		letterCodeMap[char] = ind
+		return ind
 	}
-
-	return code
+	return letterCode
 }
 
 func isIsomorphic(s string, t string) bool {
@@ -19,13 +14,12 @@ func isIsomorphic(s string, t string) bool {
 	if s == t {
 		return true
 	}
-	sCode := getLettersCode(s)
-	tCode := getLettersCode(t)
-	if len(sCode) != len(tCode) {
-		return false
-	}
-	for ind := 0; ind < len(sCode); ind++ {
-		if sCode[ind] != tCode[ind] {
+
+	letterCodeMapS := make(map[int32]int) // {letter: code}
+	letterCodeMapT := make(map[int32]int) // {letter: code}
+
+	for ind, char := range s {
+		if getLettersCode(char, ind, letterCodeMapS) != getLettersCode(int32(t[ind]), ind, letterCodeMapT) {
 			return false
 		}
 	}
