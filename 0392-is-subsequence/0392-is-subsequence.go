78af3e6b128685ffe1129d91618580e888bb9543
MaltsevaNata <42980988+MaltsevaNata@users.noreply.github.com>
2023-01-13 18:11:37 +0300

    Time: 0 ms (100.00%), Space: 1.9 MB (100.00%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.go
+++ 0392-is-subsequence/0392-is-subsequence.go
@@ -1,17 +1,19 @@
 func isSubsequence(s string, t string) bool {
 	curSubCharInd := 0
-	for ind := 0; ind < len(t); ind++ {
-		if curSubCharInd >= len(s) {
+    lenS := len(s)
+    lenT := len(t)
+	for ind := 0; ind < lenT; ind++ {
+		if curSubCharInd >= lenS {
 			return true
 		}
-		if (len(s) - curSubCharInd) > (len(t) - ind) {
+		if (lenS - curSubCharInd) > (lenT - ind) {
 			return false
 		}
 		if t[ind] == s[curSubCharInd] {
 			curSubCharInd += 1
 		}
 	}
-	if curSubCharInd == len(s) {
+	if curSubCharInd == lenS {
 		return true
 	}
 	return false
