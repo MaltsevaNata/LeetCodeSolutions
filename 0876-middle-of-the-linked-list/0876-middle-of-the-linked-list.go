525c3e601ef1bc3ec5f7e2a15c069674470c13b9
MaltsevaNata <42980988+MaltsevaNata@users.noreply.github.com>
2023-01-16 22:18:36 +0300

    Time: 0 ms (100.00%), Space: 1.9 MB (63.28%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.go
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func middleNode(head *ListNode) *ListNode {
+	var values []*ListNode
+	for head != nil {
+		values = append(values, head)
+		head = head.Next
+	}
+	res := values[len(values)/2]
+	return res
+}
