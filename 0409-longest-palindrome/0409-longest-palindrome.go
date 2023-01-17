0da20520f52c5092f0cf650ad949d242a4fe0d3d
MaltsevaNata <42980988+MaltsevaNata@users.noreply.github.com>
2023-01-17 23:38:53 +0300

    Time: 2 ms (69.84%), Space: 2 MB (54.59%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.go
+++ 0409-longest-palindrome/0409-longest-palindrome.go
@@ -1,12 +1,7 @@
 func longestPalindrome(s string) (length int) {
 	letterCounter := make(map[int32]int)
 	for _, char := range s {
-		_, ok := letterCounter[char]
-		if !ok {
-			letterCounter[char] = 1
-		} else {
-			letterCounter[char] += 1
-		}
+        letterCounter[char] += 1
 	}
 	for _, count := range letterCounter {
         length += count - count%2
