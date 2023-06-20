9c6f3b3507554647f0eeca987abac683db4ca7ea
MaltsevaNata <42980988+MaltsevaNata@users.noreply.github.com>
2023-06-21 01:22:37 +0400

    Time: 0 ms (100.00%), Space: 2 MB (26.14%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.go
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.go
@@ -7,12 +7,15 @@
  * }
  */
 func inorderTraversal(root *TreeNode) (res []int) {
-    if root == nil {
+    return nodeInorderTraversal(root, res)
+}
+
+func nodeInorderTraversal(node *TreeNode, res []int) []int {
+    if node == nil {
         return res
     }
-    res = append(res, inorderTraversal(root.Left)...)
-    res = append(res, root.Val)
-    res = append(res, inorderTraversal(root.Right)...)
-    
+    res = nodeInorderTraversal(node.Left, res)
+    res = append(res, node.Val)
+    res = nodeInorderTraversal(node.Right, res)
     return res
 }
