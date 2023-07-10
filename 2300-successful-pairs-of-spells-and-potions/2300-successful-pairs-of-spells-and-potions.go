4102ecef6124ef9df9a5a9bfa65d2b41f04d7dc4
MaltsevaNata <42980988+MaltsevaNata@users.noreply.github.com>
2023-07-10 22:45:25 +0400

    Time: 232 ms (35.13%), Space: 13.5 MB (20.27%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.go
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.go
@@ -1,7 +1,5 @@
 func successfulPairs(spells []int, potions []int, success int64) []int { // 7
-    sort.Slice(potions, func(i, j int) bool { // [1,2,3,4,5]
-        return potions[i] < potions[j]
-    }) // nlogn
+    sort.Ints(potions) // nlogn
     
     var result = make([]int, len(spells))
     var ready = make(map[int]int)
@@ -11,24 +9,20 @@ func successfulPairs(spells []int, potions []int, success int64) []int { // 7
             result[idx] = val
             continue
         }
-        
-        minMult := success / int64(spell) // 7/1 = 7
-        if success % int64(spell) > 0 { // 0
-            minMult++
-        }
+        minMult := int(math.Ceil(float64(success)/float64(spell)))
         
         var left, right int
         right = len(potions) - 1 // 4
         
-        if int64(potions[right]) < minMult {
+        if potions[right] < minMult {
             ready[spell] = 0
             continue
         }
         
         for { // logn
             mid := (left + right) / 2 // 1
-            if int64(potions[mid]) >= minMult  { 
-                if mid == 0 || int64(potions[mid-1]) < minMult  {
+            if potions[mid] >= minMult  { 
+                if mid == 0 || potions[mid-1] < minMult  {
                     result[idx] = len(potions) - mid // 5 - 1 = 4
                     break
                 }
